584f907d0a31e408ed411ba8026d1fb7804775d9
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2024-11-28 21:51:33 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "c3a451d0adt46fobb2b9a77755f49315";
   let context =
-    "You are a drama Poem expert and love to write short poems. You should generate a 6 line poem and separate each line with a <br />. Make sure to follow user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a drama Poem expert and love to write short poems. You should generate a 5 line poem and separate each line with a <br />. Make sure to follow user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a Ukrainian poem about ${instructionsInput.value}`;
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
